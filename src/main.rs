ab3d023a038b5d0f4d3cdd74afdf2ad1f68a08ff
Michael Gattozzi <mgattozzi@gmail.com>
2020-08-29 11:46:20 -0400

    Parallelize code and prepare for pretty printing
    These changes do a few things that are important. We first parallelize
    the code so that recursive traversal of large files will be
    significantly faster using rayon, with negligible perf hit in the small
    file case. We also switch our JsonPath type to use im::Vector so that
    clones will be cheap as we need those rather than &mut to work in
    parallel. We also setup a channel to receive messages in parallel across
    all the threads that might be spun up. What we send is the new Diff type
    which we can later use to setup pretty printing of the data with say
    a table or something like that.

--- src/main.rs
+++ src/main.rs
@@ -1,139 +1,185 @@
 use color_eyre::eyre::Result;
+use crossbeam::crossbeam_channel::{self, Sender};
+use im::Vector;
+use rayon::iter::ParallelBridge;
+use rayon::prelude::*;
+use serde_json::Value;
 use std::{fmt, fs, path::PathBuf};
 use structopt::StructOpt;
-use serde_json::Value;
 use tracing_subscriber;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-  #[structopt(parse(from_os_str))]
-  first: PathBuf,
-  #[structopt(parse(from_os_str))]
-  second: PathBuf,
+    #[structopt(parse(from_os_str))]
+    first: PathBuf,
+    #[structopt(parse(from_os_str))]
+    second: PathBuf,
 }
 
 fn main() -> Result<()> {
-  color_eyre::install()?;
-  tracing_subscriber::fmt::init();
-  let opt = Opt::from_args();
+    color_eyre::install()?;
+    tracing_subscriber::fmt::init();
+    let opt = Opt::from_args();
 
-  let first: Value =
-    serde_json::from_str(&fs::read_to_string(opt.first)?)?;
-  let second: Value =
-    serde_json::from_str(&fs::read_to_string(opt.second)?)?;
+    let first: Value = serde_json::from_str(&fs::read_to_string(opt.first)?)?;
+    let second: Value = serde_json::from_str(&fs::read_to_string(opt.second)?)?;
 
-  let mut path = JsonPath::new();
-  json(&first, &second, &mut path)
+    let path = JsonPath::new();
+    let (tx, rx) = crossbeam_channel::unbounded();
+    json(&first, &second, path, tx);
+
+    for diff in rx.iter() {
+        println!("{}", diff);
+    }
+    Ok(())
 }
 
-fn json(left: &Value, right: &Value, path: &mut JsonPath) -> Result<()> {
-  match (left, right) {
-    (Value::Object(left_map), Value::Object(right_map)) => {
-      for (key, lvalue) in left_map.iter() {
-        path.obj(key);
-        if let Some(rvalue) = right_map.get(key) {
-            json(lvalue, rvalue, path)?;
-        } else {
-          println!("{} is not in the rvalue", path);
+fn json(left: &Value, right: &Value, path: JsonPath, tx: Sender<Diff>) {
+    match (left, right) {
+        (Value::Object(left_map), Value::Object(right_map)) => {
+            left_map.iter().par_bridge().for_each(|(key, lvalue)| {
+                let mut path = path.clone();
+                path.obj(key);
+                if let Some(rvalue) = right_map.get(key) {
+                    json(lvalue, rvalue, path, tx.clone());
+                } else {
+                    tx.send(Diff {
+                        path,
+                        kind: DiffKind::RvalueDNE,
+                    })
+                    .unwrap();
+                }
+            });
+            right_map.keys().par_bridge().for_each(|rvalue| {
+                let mut path = path.clone();
+                path.obj(rvalue);
+                if left_map.get(rvalue).is_none() {
+                    tx.send(Diff {
+                        path,
+                        kind: DiffKind::LvalueDNE,
+                    })
+                    .unwrap();
+                }
+            });
         }
-        path.pop();
-      }
-      for key in right_map.keys() {
-        path.obj(key);
-        if left_map.get(key).is_none() {
-          println!("{} is not in the lvalue", path);
+        (Value::Array(left_array), Value::Array(right_array)) => {
+            //let order_matters = true;
+            left_array
+                .iter()
+                .zip(right_array)
+                .enumerate()
+                .par_bridge()
+                .for_each(|(i, (lvalue, rvalue))| {
+                    let mut path = path.clone();
+                    path.array(i);
+                    json(lvalue, rvalue, path.clone(), tx.clone());
+                });
+
+            let llen = left_array.len();
+            let rlen = right_array.len();
+
+            if llen != rlen {
+                if llen > rlen {
+                    tx.send(Diff {
+                        path: path.clone(),
+                        kind: DiffKind::LMoreElements(llen - rlen),
+                    })
+                    .unwrap();
+                } else {
+                    tx.send(Diff {
+                        path: path.clone(),
+                        kind: DiffKind::RMoreElements(rlen - llen),
+                    })
+                    .unwrap();
+                }
+            }
         }
-        path.pop();
-      }
-    }
-    (Value::Array(left_array), Value::Array(right_array)) => {
-      //let order_matters = true;
-      path.array(0);
-      for (lvalue, rvalue) in left_array.iter().zip(right_array) {
-        json(lvalue, rvalue, path)?;
-        path.inc_array();
-      }
-      path.pop();
-
-      let llen = left_array.len();
-      let rlen = right_array.len();
-
-      if llen != rlen {
-        if llen > rlen {
-          println!("The lvalue {} has {} more elements than the rvalue", path, llen - rlen);
-
-        } else {
-          println!("The rvalue {} has {} more elements than the lvalue", path, rlen - llen);
+        (lvalue, rvalue) => {
+            if lvalue != rvalue {
+                tx.send(Diff {
+                    path: path.clone(),
+                    kind: DiffKind::DiffValue(lvalue.clone(), rvalue.clone()),
+                })
+                .unwrap();
+            }
         }
-      }
     }
-    (Value::String(lvalue), Value::String(rvalue)) => {
-      if lvalue != rvalue {
-        println!("{} => {} <> {}", path, lvalue, rvalue);
-      }
-    },
-    (Value::Number(lvalue), Value::Number(rvalue)) => {
-      if lvalue != rvalue {
-        println!("{} => {} <> {}", path, lvalue, rvalue);
-      }
-    },
-    (Value::Bool(lvalue), Value::Bool(rvalue)) => {
-      if lvalue != rvalue {
-        println!("{} => {} <> {}", path, lvalue, rvalue);
-      }
-    },
-    (Value::Null, Value::Null) => {}
-    (lvalue, rvalue) => {
-        println!("{} => {} <> {}", path, lvalue, rvalue);
-    }
-  }
-  Ok(())
 }
+
+struct Diff {
+    path: JsonPath,
+    kind: DiffKind,
+}
+
+enum DiffKind {
+    LvalueDNE,
+    RvalueDNE,
+    DiffValue(Value, Value),
+    LMoreElements(usize),
+    RMoreElements(usize),
+}
+
+impl fmt::Display for Diff {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.kind {
+            DiffKind::LvalueDNE => write!(f, "{} is not in the lvalue", self.path),
+            DiffKind::RvalueDNE => write!(f, "{} is not in the rvalue", self.path),
+            DiffKind::DiffValue(lvalue, rvalue) => {
+                write!(f, "{} => {} <> {}", self.path, lvalue, rvalue)
+            }
+            DiffKind::LMoreElements(n) => write!(
+                f,
+                "The lvalue {} has {} more elements than the rvalue",
+                self.path, n
+            ),
+            DiffKind::RMoreElements(n) => write!(
+                f,
+                "The rvalue {} has {} more elements than the lvalue",
+                self.path, n
+            ),
+        }
+    }
+}
+
+#[derive(Clone)]
 struct JsonPath {
-  path: Vec<JsonType>
+    path: Vector<JsonType>,
 }
 
+#[derive(Clone)]
 enum JsonType {
-  Object(String),
-  Array(usize),
+    Object(String),
+    Array(usize),
 }
 
 impl JsonPath {
-  fn new() -> Self {
-    Self { path: Vec::new() }
-  }
-  fn obj<T: Into<String>>(&mut self, input: T) {
-    self.path.push(JsonType::Object(input.into()));
-  }
-  fn array(&mut self, input: usize) {
-    self.path.push(JsonType::Array(input));
-  }
-  fn pop(&mut self) {
-    self.path.pop();
-  }
-  fn inc_array(&mut self) {
-    if let Some(v) = self.path.last_mut() {
-      let x = match v {
-        JsonType::Array(i) => *i + 1,
-        _ => panic!("Oh no this not array"),
-      };
-      *v = JsonType::Array(x);
+    fn new() -> Self {
+        Self {
+            path: Vector::new(),
+        }
     }
-  }
+    fn obj<T: Into<String>>(&mut self, input: T) {
+        self.path.push_back(JsonType::Object(input.into()));
+    }
+    fn array(&mut self, input: usize) {
+        self.path.push_back(JsonType::Array(input));
+    }
 }
 
 impl fmt::Display for JsonPath {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    for (i, value) in self.path.iter().enumerate() {
-      match value {
-        JsonType::Array(index) => write!(f, "[{}]", index)?,
-        JsonType::Object(field) => if i == 0 {
-          write!(f, "{}", field)?
-        } else {
-          write!(f, ".{}", field)?
-        },
-      }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (i, value) in self.path.iter().enumerate() {
+            match value {
+                JsonType::Array(index) => write!(f, "[{}]", index)?,
+                JsonType::Object(field) => {
+                    if i == 0 {
+                        write!(f, "{}", field)?
+                    } else {
+                        write!(f, ".{}", field)?
+                    }
+                }
+            }
+        }
+        Ok(())
     }
-    Ok(())
-  }
 }
